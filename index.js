381fef01c33fe2fc45be52549ebc1ee73f92c556
Facundo Alvarez <turcoalv@gmail.com>
2020-07-06 17:55:26 -0300

    [INITIAL COMIT] Startup file created

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+const router = require("./src/routes");
+
+server.set("port", 5000);
+server.use(router);
+server.listen(server.get("port"), () => {
+  console.log(`Server running on port ${server.get("port")}`);
+});
