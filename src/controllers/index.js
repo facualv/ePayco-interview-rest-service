7077cbb8113864fe3579e0d60d7cc44d76407a13
Facundo Alvarez <turcoalv@gmail.com>
2020-07-06 17:58:06 -0300

    [ADD] Basic Conrollers Templates added

---
+++ src/controllers/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AuthController: require("./auth.controller"),
+  TransactionController: require("./transaction.controller"),
+  WalletController: require("./wallet.controller")
+};
