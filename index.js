e715ae3f03e0a983d55c27fef785f5fce67c8178
Facundo Alvarez <turcoalv@gmail.com>
2020-07-07 20:50:16 -0300

    [ADD] soap request library added

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-const { PORT, APPLICATION_NAME } = require('./src/config')
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const { PORT, APPLICATION_NAME } = require('./src/config');
+require('express-async-errors');
 
 const server = express();
-const router = require("./src/routes");
+const router = require('./src/routes');
 
-server.set("port", PORT);
+server.set('port', PORT);
 server.use(router);
 server.listen(PORT, () => {
   console.log(`${APPLICATION_NAME} running on port ${PORT}`);
