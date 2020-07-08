094b9f4135ebb7b4cdb7a5368af0351e75002a72
Facundo Alvarez <turcoalv@gmail.com>
2020-07-08 20:57:47 -0300

    [FIX] sintax error

--- src/helpers/xmlRequestBody.helper.js
+++ src/helpers/xmlRequestBody.helper.js
@@ -1,9 +1,9 @@
-const generateXmlLoginBody = function (name) {
+const generateXmlLoginBody = function (email) {
   return (xmlResult = `<?xml version="1.0" encoding="utf-8"?>
    <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
      <soap12:Body>
        <login xmlns="http://tempuri.org/">
-         <email>${name}</email>\
+         <email>${email}</email>\
        </login>
      </soap12:Body>
    </soap12:Envelope>`);
