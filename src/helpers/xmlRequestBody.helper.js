79067940b58c7c55b6862105bab64c7e4400453c
Facundo Alvarez <turcoalv@gmail.com>
2020-07-08 18:04:01 -0300

    [ADD] xml body signup helper added

--- src/helpers/xmlRequestBody.helper.js
+++ src/helpers/xmlRequestBody.helper.js
@@ -1,15 +1,30 @@
-const getLoginXmlBody = function (name, password) {
+const generateXmlLoginBody = function (name) {
   return (xmlResult = `<?xml version="1.0" encoding="utf-8"?>
    <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
      <soap12:Body>
        <login xmlns="http://tempuri.org/">
-         <email>${name}</email>
-         <password>${password}</password>
+         <email>${name}</email>\
        </login>
      </soap12:Body>
    </soap12:Envelope>`);
 };
 
+const generateXmlSignUpBody = function (clientId, name, phone, email, password) {
+  return (xmlResult = `<?xml version="1.0" encoding="utf-8"?>\
+  <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">\
+    <soap12:Body>\
+      <signup xmlns="http://tempuri.org/">\
+        <clientId>${clientId}</clientId>\
+        <name>${name}</name>\
+        <phone>${phone}</phone>\
+        <email>${email}</email>\
+        <password>${password}</password>\
+      </signup>\
+    </soap12:Body>\
+  </soap12:Envelope>`);
+};
+
 module.exports = {
-  getLoginXmlBody
+  generateXmlLoginBody,
+  generateXmlSignUpBody
 };
