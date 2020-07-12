8e78b1dae19b06f938530666a0dd7611c0890be7
Facundo Alvarez <turcoalv@gmail.com>
2020-07-12 14:22:30 -0300

    [ADD] transaction controller added

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,50 +1,23 @@
 const sh = require('shorthash');
 const { EmailSender } = require('../helpers');
-const { TransactionService } = require('../services');
-let storedToken = '';
+const {
+  TransactionService: { paymentService, rechargeService }
+} = require('../services');
 
 async function payment(req, res) {
-  const { ammount, detail, sessionId, token } = req.body;
-  const email = req.session.email;
-  const sessId = req.session.id;
+  const paymentData = req.body;
 
-  if (!sessionId && !token && email) {
-    console.log(req.session);
-
-    const email = req.session.email;
-    console.log(email);
-
-    //Generate a token
-    storedToken = sh.unique(JSON.stringify(req.body));
-    console.log(storedToken);
-
-    //Saves the generated token
-    EmailSender(email, storedToken, sessId);
-    res.send({ message: 'Token and SessionId Sended to your email' });
-  }
-
-  if (ammount && detail && sessionId && token) {
-    console.log(sessionId);
-
-    if (storedToken == token && sessionId == sessId) {
-      // calls the data access payment function
-      res.send({ message: 'VALID TOKEN AND SESSION ID' });
-    } else {
-      res.send({ message: 'INVALID TOKEN OR SESSION ID' });
-      res.status(403);
-    }
-  }
+  //Adds the clienId to the payment data object
+  paymentData.clientId = req.session.clientId;
+  const payementResponse = await paymentService(paymentData);
+  res.send(payementResponse);
 }
 
 async function recharge(req, res) {
-  const { clientId, phone, ammount } = req.body;
-
-  if (clientId && phone && ammount) {
-    // Sends the credentials to the auth service expecting a client object id they are valid
-    res.json({ message: 'Recharge Route' });
-  } else {
-    res.status(400);
-  }
+  const rechargeData = req.body;
+  const rechargeResponse = await rechargeService(rechargeData);
+  console.log(rechargeResponse);
+  return res.send(rechargeResponse);
 }
 
 module.exports = {
