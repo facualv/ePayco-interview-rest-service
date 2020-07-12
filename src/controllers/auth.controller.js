d6f430883f9c8870479d525685f1891ac160acee
Facundo Alvarez <turcoalv@gmail.com>
2020-07-12 14:12:36 -0300

    [REFACTOR] put bussines logic out of controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,36 +1,30 @@
-const { AuthService } = require('../services');
+const {
+  AuthService: { signUpService, loginService }
+} = require('../services');
 
-module.exports = {
-  signIn: async (req, res) => {
-    const { name, clientId, phone, email, password } = req.body;
-    if (name && phone && email && password && clientId) {
-      const newClient = req.body;
-      console.log(newClient);
-      await AuthService.signUp(newClient);
-      res.json({ added: 'ok' });
-    } else {
-      res.status(400);
-    }
-  },
-  login: async (req, res) => {
-    const { email, password } = req.body;
+async function signUp(req, res) {
+  const newClient = req.body;
+  const signUpResponse = await signUpService(newClient);
+  console.log(signUpResponse);
 
-    if (email && password) {
-      // Sends the credentials to the auth service expecting a client object id they are valid
-      const { Client } = await AuthService.logIn(email, password);
-      res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.send(signUpResponse);
+}
 
-      // If the client is not null it means that it was succesfully validated
-      if (Client) {
-        req.session.clientId = Client.ClientId;
-        req.session.email = Client.Email;
-        req.session.phone = Client.Phone
-        console.log(req.session);
-        // The session middleware is working properly
-        res.json({ message: 'Login Successful' });
-      }
-    } else {
-      res.status(400);
-    }
+async function login(req, res) {
+  const newLogin = req.body;
+  const loginResponse = await loginService(newLogin);
+  //If there is a not falsy response from the login service the we create the session cookie
+  // with al the data needed to perform a payment
+  if (loginResponse) {
+    const {
+      client: { ClientId, Email, Phone }
+    } = loginResponse;
+
+    req.session.clientId = ClientId;
+    req.session.email = Email;
+    req.session.phone = Phone;
   }
-};
+  res.send(loginResponse);
+}
+
+module.exports = { signUp, login };
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,49 +1,54 @@
 const { compareSync, hashSync, genSaltSync } = require('bcryptjs');
-const dataAccess = require('../data-access');
+const { AuthDataAccess } = require('../data-access');
 
-async function signUp(newClient) {
-  const { email } = newClient;
+async function signUpService(newClient) {
+  const { clientId, name, phone, email, password } = newClient;
 
-  //Checks if there is a client with the email
-  const response = await dataAccess.LogIn(email);
-  const { Client } = response;
-
-  if (Client) {
+  //Checks if the client is sending all the data to sign up
+  if (!clientId || !name || !phone || !email || !password) {
     const error = new Error();
-    error.message = 'Client already exist';
+    error.status = 400;
+    error.message = 'All the client data must be sent';
+    throw error;
+  }
+
+  //Validates is the client is indeed a new client (THIS SHOULD BE CHANGE!!!!!!!!!!!!)
+  const validationResponse = await AuthDataAccess.login(email);
+  const { error, message } = validationResponse;
+
+  //If the error is false it means that there is already a client with the same credentials
+  if (!error) {
+    const error = new Error();
+    error.message = 'Another client is register with the same email, phone or client id';
     error.status = 401;
     throw error;
   }
 
   //Here comes the encryptyon of the password
   const salt = genSaltSync(10);
-  const hashedPassword = hashSync(newClient.password, salt);
-  newClient.password = hashedPassword;
+  const hashedPassword = hashSync(password, salt);
 
   // Sends the data to create a client and returns a response (check the response format in DATA ACCESS LAYER)
-  return await dataAccess.SignUp(newClient);
+  return AuthDataAccess.signUp(clientId, name, phone, email, hashedPassword);
 }
 
-async function logIn(email, password) {
-  //Searchs the client by email
-  const response = await dataAccess.LogIn(email);
-
-  //Extracts the client from the response
-  const { Client } = response;
-
+async function loginService(newLogin) {
+  const { email, password } = newLogin;
+  //Checks if there is a client with that email
+  const response = await AuthDataAccess.login(email);
+  //Extracts the client if exists, the message and IsError from the response
+  const { error, message, client } = response;
   // If the client doesnt exists it returns an error
-  if (!Client) {
+  if (!client && error) {
     const error = new Error();
-    error.message = 'Client doesnt not exist';
+    error.message = message;
     error.status = 404;
     throw error;
   }
 
-  // console.log(Client);
-
   //It compares the passwords
   const loginPassword = password;
-  const storedPassword = Client.Password;
+  const storedPassword = client.Password;
   const validPassword = compareSync(loginPassword, storedPassword);
 
   if (!validPassword) {
@@ -52,11 +57,10 @@ async function logIn(email, password) {
     error.status = 404;
     throw error;
   }
-
   return response;
 }
 
 module.exports = {
-  logIn,
-  signUp
+  loginService,
+  signUpService
 };
